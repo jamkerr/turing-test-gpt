26d149dbf7fa3971f0f1d693082d915f753bc180
jamkerr <kerr0088@gmail.com>
2023-03-06 14:29:04 +0100

    Add dark mode and some test styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
     import "../app.css";
+
+    let dark = true;
+
 </script>
-  
-<slot />
+
+<div class:dark>
+    <nav>
+        <a href="/">home</a>
+        <a href="/" on:click|preventDefault={ () => {
+            dark = !dark;
+        }}>{dark ? 'dark' : 'light'}</a>
+    </nav>
+
+    <main class="min-h-screen bg-white dark:bg-slate-800">
+        <slot />
+    </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,17 @@
     let text: string = 'And here be the text.';
 </script>
 
-<div class="place-content-center">
+<div class="flex flex-col items-center p-5 gap-1 text-slate-900 dark:text-white">
     <h1 class="text-3xl font-bold">Is it me or GPT?</h1>
     <p>Can you tell whether a piece of writing was written by me (a human, as far as I can tell), or ChatGPT (an AI)?</p>
-    <p>We both responded to the same prompts. Below, you'll see some of our creations, and you get get to guess who wrote the text: was me or ChatGPT?</p>
+    <p>We both responded to the same prompts. Below, you'll see some of our creations, and you get to guess who wrote the text: was me or ChatGPT?</p>
     <h2 class="text-2xl font-bold">Prompt</h2>
     <p>{prompt}</p>
     <h2 class="text-2xl font-bold">Text</h2>
     <p>{text}</p>
 
-    <button>It's you</button>
-    <button>It's GPT</button>
+    <div class="flex flex-row gap-4">
+        <button class="border-2 rounded-lg bg-pink-400 p-1">It's you</button>
+        <button class="border-2 rounded-lg bg-pink-400 p-1">It's GPT</button>
+    </div>
 </div>
